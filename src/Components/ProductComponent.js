1f46b24e87e48a12e66479a70995b39a3799f2fc
Abdullrahman Ayman <abdullrahman.ayman@our-edu.net>
2022-08-04 16:17:38 +0200

    shopping_app

---
+++ src/Components/ProductComponent.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { selectedProduct } from "./redux/actions/productActions";
+import { Link } from "react-router-dom";
+
+const ProductComponent = (state) => {
+  const products = useSelector((state) => state.allProducts.products);
+  const Mydispatch = useDispatch();
+
+  const selectProduct = (id) => {
+    Mydispatch(selectedProduct(products[id]));
+  };
+  //  let myCart = useSelector((state) => state.shop.cart)
+  //   if (myCart.length > 0 ){myCart.splice(0, myCart.length)
+  //   }
+
+  return (
+    <Fragment>
+      <div className="container col-10">
+        <div className="row mt-4">
+          {products.map((product, id) => (
+            <div
+              className="card col-sm-2 mt-4"
+              key={product.id}
+              onClick={() => selectProduct(id)}
+            >
+              <Link className="cardInfo" to={`/product/${product.id}`}>
+                <img
+                  src={product.image}
+                  className="card-img-top"
+                  alt={product.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <div className="myDetails">Details</div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ProductComponent;
